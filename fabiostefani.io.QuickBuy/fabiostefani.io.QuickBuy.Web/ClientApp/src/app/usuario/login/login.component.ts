366d16838012c0c60d6b4fc504d742099d6fd210
Fabio de Stefani <fabiostefani@gmail.com>
2019-10-10 07:07:07 -0300

    Componente de Login

--- fabiostefani.io.QuickBuy/fabiostefani.io.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ fabiostefani.io.QuickBuy/fabiostefani.io.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: "app-login"
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
+
+export class LoginComponent {
+    public email = "fabio@teste.com";
+    public senha = "1254";
+
+    entrar() {
+        alert(this.email + this.senha);
+    }
+}
