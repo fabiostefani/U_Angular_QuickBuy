bb27c1fcbbc5f45a88e752f999bada68ccd76bbc
Fabio de Stefani <fabiostefani@gmail.com>
2019-10-12 06:52:09 -0300

    Estrutura de Autenticação

--- fabiostefani.io.QuickBuy/fabiostefani.io.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ fabiostefani.io.QuickBuy/fabiostefani.io.QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { Usuario } from 'src/app/modelo/usuario';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -11,13 +12,14 @@ export class LoginComponent {
     public usuario: Usuario;
     public usuarioAutenticado: boolean;
 
-    constructor() {
+    constructor(private router: Router) {
         this.usuario = new Usuario();
     }
 
     entrar() {
         if (this.usuario.email == "fabio@teste.com" && this.usuario.senha == "123654") {
-            this.usuarioAutenticado = true;
+            localStorage.setItem("usuario-autenticado", "1");
+            //this.router.navigate(['/']);
         }
     }
 }
